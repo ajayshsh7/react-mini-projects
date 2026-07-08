707574c21cd41c5621f00c12958b7bca79d76731
ajayshsh7 <ajayshsh7@gmail.com>
2026-07-08 11:48:51 +0530

    todo completed

--- mini-react-p/src/components/simple-todo/Todo.jsx
+++ mini-react-p/src/components/simple-todo/Todo.jsx
@@ -2,18 +2,65 @@ import { useState } from 'react'
 
 
 function Todo(){
+    const [newItem, setNewItem] = useState("");
+    const [todo, setTodo] = useState([]);
 
 
+    function handleSubmit(e) {
+        e.preventDefault();
+
+        setTodo(currentTodo => {
+            return [
+                ...currentTodo, 
+                {
+                    id: crypto.randomUUID(), title: newItem, completed: false
+                },
+            ]
+        })
+        setNewItem("");
+    }
+
+    function toggleTodo(id, completed){
+        setTodo(currentTodo =>{
+            return currentTodo.map(todos =>{
+                if(todos.id === id) {
+                    return {...todos, completed}
+                }
+                return todos
+            })
+        })
+    }
+
+    function deleteTodo(id){
+        setTodo(currentTodo =>{
+            return currentTodo.filter(todos => todos.id !== id)
+        })
+    }
 
     return(
         <>
-        <form>
+        <form onSubmit={handleSubmit}>
             <div className='form'>
                 <label htmlFor="item"> Add Notes</label>
-                <input type="text" id='item' />
+                <input value={newItem} 
+                onChange={e => setNewItem(e.target.value)}
+                type="text" id='item' />
             </div>
             <button className='btn'>Add</button>
         </form>
+        <h1>My Notes</h1>
+        <ul>
+            {todo.map(todos =>{
+                return <li key={todos.id} style={{ listStyleType: "none" }}>
+                    <label htmlFor="">
+                        <input type="checkBox" checked={todos.completed} 
+                        onChange={e => toggleTodo(todos.id, e.target.checked)}/>
+                        {todos.title}
+                    </label>
+                    <button onClick={() =>deleteTodo(todos.id)}>Delete</button>
+                </li>
+            })}
+        </ul>
         </>
     );
 }
