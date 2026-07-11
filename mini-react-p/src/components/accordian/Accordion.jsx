bb66a1df224261b251778c221b8ee39e8aa5a7b5
ajayshsh7 <ajayshsh7@gmail.com>
2026-07-11 12:06:45 +0530

    Accordion completed

--- mini-react-p/src/components/accordian/Accordion.jsx
+++ mini-react-p/src/components/accordian/Accordion.jsx
@@ -2,26 +2,39 @@ import { useState } from 'react'
 
 
 function Accordion() {
+
+    const [activeItem, setActiveItem] = useState(null);
+
     const faq = [
         {
             id: 1,
-            title: "Title",
-            desc: "This is Description 1",
+            title: "What is an Accordion in React?",
+            desc: "An Accordion is a UI component that displays a list of items where users can expand or collapse each item to show or hide its content. It helps organize information and saves screen space by displaying only the necessary content.",
         },
         {
             id: 2,
-            title: "Title 2",
-            desc: "This is Description 2",
+            title: "How does an Accordion work?",
+            desc: "An Accordion works by storing the currently opened item in React state using 'useState'. When a user clicks on a question:",
         },
         {
             id: 3,
-            title: "Title 3",
-            desc: "This is Description 3",
+            title: "What React concepts are used in the Accordion project?",
+            desc: "This project uses several core React concepts like Functional Components, JSX, useState, Event Hanlding, Conditional Rendering, Dynamic UI updates, Rendering Lists etc.",
+        },
+        {
+            id: 4,
+            title: "Why is 'useState' used in the Accordion?",
+            desc: "'useState' is used to remember which accordion item is currently open. Without state, React would not know which description should be displayed.",
+        },
+        {
+            id: 5,
+            title: "Why do we use conditional rendering in an Accordion?",
+            desc: "Conditional rendering allows React to display content only when a specific condition is true. If the item's id matches activeItem, React displays the description. Otherwise, nothing is rendered. This creates the expand and collapse behavior.",
         },
     ]
     
-    function handleClick(){
-        
+    const handleClick = (id) =>{
+        setActiveItem((prev) => (prev === id ? null : id));
     }
 
   return (
@@ -34,8 +47,12 @@ function Accordion() {
             borderRadius: "10px",
             boxShadow: "0 4px 10px rgba(0, 0, 0, 0.1)",
         }}>
-            <h2 onClick={handleClick}>{faqs.title}</h2>
-            <p>{faqs.desc}</p>
+            <h2 style={{border: "none",
+  background: "transparent",
+  cursor: "pointer",}} onClick={ () => handleClick(faqs.id)}>{faqs.title}</h2> 
+            <div style={{padding: "1em", maxWidth: "50ch"}}>
+                {activeItem === faqs.id && <div>{faqs.desc}</div>}
+            </div>
         </div>
     ))}
     </>
